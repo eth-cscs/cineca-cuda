7154baa085478b930e74ad9d497022dc31cd965e
bcumming <bcumming@cscs.ch>
2016-02-11 16:34:36 +0100

    use powers of 2 for cublas example

--- exercises/axpy/axpy_cublas.cu
+++ exercises/axpy/axpy_cublas.cu
@@ -5,10 +5,11 @@
 #include "util.h"
 
 int main(int argc, char** argv) {
-    size_t N = read_arg(argc, argv, 1, 16);
-    auto size_in_bytes = N * sizeof(double);
+    size_t pow = read_arg(argc, argv, 1, 16);
+    size_t n = 1 << pow;
+    auto size_in_bytes = n * sizeof(double);
 
-    std::cout << "memcopy and daxpy test of length N = " << N
+    std::cout << "memcopy and daxpy test of length n = " << n
               << " : " << size_in_bytes/(1024.*1024.) << "MB"
               << std::endl;
 
@@ -17,30 +18,32 @@ int main(int argc, char** argv) {
     // initialize cublas
     auto cublas_handle = get_cublas_handle();
 
-    double* x_device = malloc_device<double>(N);
-    double* y_device = malloc_device<double>(N);
+    double* x_device = malloc_device<double>(n);
+    double* y_device = malloc_device<double>(n);
 
-    double* x_host = malloc_host<double>(N, 1.5);
-    double* y_host = malloc_host<double>(N, 3.0);
-    double* y      = malloc_host<double>(N, 0.0);
+    double* x_host = malloc_host<double>(n, 1.5);
+    double* y_host = malloc_host<double>(n, 3.0);
+    double* y      = malloc_host<double>(n, 0.0);
+
+    copy_to_device<double>(x_host, x_device, n);
 
     // copy to device
     auto start = get_time();
-    copy_to_device<double>(x_host, x_device, N);
-    copy_to_device<double>(y_host, y_device, N);
+    copy_to_device<double>(x_host, x_device, n);
+    copy_to_device<double>(y_host, y_device, n);
     auto time_H2D = get_time() - start;
 
     // y = y + 2 * x
     start = get_time();
     double alpha = 2.0;
     auto cublas_status =
-        cublasDaxpy(cublas_handle, N, &alpha, x_device, 1, y_device, 1);
+        cublasDaxpy(cublas_handle, n, &alpha, x_device, 1, y_device, 1);
     cudaDeviceSynchronize();
     auto time_axpy = get_time() - start;
 
     // copy result back to host
     start = get_time();
-    copy_to_host<double>(y_device, y, N);
+    copy_to_host<double>(y_device, y, n);
     auto time_D2H = get_time() - start;
 
     std::cout << "-------\ntimings\n-------" << std::endl;
@@ -51,7 +54,7 @@ int main(int argc, char** argv) {
     // check for errors
     auto errors = 0;
     #pragma omp parallel for reduction(+:errors)
-    for(auto i=0; i<N; ++i) {
+    for(auto i=0; i<n; ++i) {
         if(std::fabs(6.-y[i])>1e-15) {
             errors++;
         }
