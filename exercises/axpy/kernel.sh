61b235bfe1f8c13c421a1e6ab81d5bb759f91dc4
Ben Cumming <bcumming@cscs.ch>
2016-02-18 20:59:46 +0100

    add slides and PBS scripts

---
+++ exercises/axpy/kernel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -A train_scA2016
+#PBS -l select=1:ncpus=1:ngpus=1
+#PBS -q parallel
+#PBS -W group_list=train_scA2016
+
+module load gnu cuda
+
+nvidia-smi
+
+./axpy_kernel.cuda 8
+
